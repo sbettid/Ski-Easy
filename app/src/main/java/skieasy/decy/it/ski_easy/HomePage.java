81fc41372fffce54f85274536a9ef641d75cf04b
CremoD <cremo.davide96@gmail.com>
2017-04-07 13:39:49 +0200

    NavigationBar icons

--- app/src/main/java/skieasy/decy/it/ski_easy/HomePage.java
+++ app/src/main/java/skieasy/decy/it/ski_easy/HomePage.java
@@ -39,6 +39,7 @@ public class HomePage extends AppCompatActivity {
 
         mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
