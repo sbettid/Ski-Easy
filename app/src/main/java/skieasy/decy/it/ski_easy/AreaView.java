76fa7b04b3a45ad9a2467fa6e71449bb67cbd4f8
Davide <davide.sbetti@gmail.com>
2017-04-19 15:19:06 +0200

    Temp commit

--- app/src/main/java/skieasy/decy/it/ski_easy/AreaView.java
+++ app/src/main/java/skieasy/decy/it/ski_easy/AreaView.java
@@ -3,7 +3,17 @@ package skieasy.decy.it.ski_easy;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class AreaView extends AppCompatActivity {
 
@@ -12,10 +22,21 @@ public class AreaView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_area_view);
 
-
+        //Get area
         TextView title = (TextView) findViewById(R.id.areaTitle);
         Intent current = getIntent();
         String areaName = current.getStringExtra("name");
         title.setText(areaName);
+
+
+       Object obj =  new WeatherForecasts().execute();
+
+       if(obj instanceof String)
+           Log.d("WEATHER", "String!");
+
+
+
+
+        //Log.d("WEATHER","json: " + json );
     }
 }
---
+++ app/src/main/java/skieasy/decy/it/ski_easy/WeatherForecasts.java
@@ -0,0 +1,65 @@
+package skieasy.decy.it.ski_easy;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Created by Davide on 19/04/2017.
+ */
+
+class WeatherForecasts extends AsyncTask{
+    @Override
+    protected Object doInBackground(Object[] params) {
+
+        //Load weather forecasts from openwather.com using their API
+        String apikey = "80634df8f9b5e5e561d6ac78eadba130";
+        String urlAddress = "http://api.openweathermap.org/data/2.5/forecast/daily";
+        String response = "";
+        String fullURL = urlAddress + "?APPID=" + apikey + "&q=bolzano";
+
+        String json = "";
+
+        try {
+            URL myUrl = new URL(fullURL);
+
+            URLConnection myConn = myUrl.openConnection();
+
+            Log.d("WEATHER", "" + myConn.getURL());
+            HttpURLConnection myHttp = null;
+
+            if(myConn instanceof HttpURLConnection){
+                myHttp = (HttpURLConnection) myConn;
+            }else{
+                Log.d("WEATHER", "Problem!!");
+            }
+
+            BufferedReader myIn = new BufferedReader(new InputStreamReader( myHttp.getInputStream()) ) ;
+
+            while((response = myIn.readLine()) != null)
+            json+=response;
+
+
+        } catch (MalformedURLException e) {
+            Log.d("WEATHER", "Malformed URL");
+        } catch (IOException e) {
+            Log.d("WEATHER", "IOEXception");
+        }
+
+
+        return json;
+    }
+
+    @Override
+    protected void onPostExecute(Object par){
+            
+    }
+
+}
